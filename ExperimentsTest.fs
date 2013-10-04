0dfb66dffb972a06d2fb6fd663476188d8c6c6dd
BudgieInWA <benjaminaritter@gmail.com>
2013-10-04 15:09:49 +0800

    Changed var names to indicate the function they're in

--- ExperimentsTest.fs
+++ ExperimentsTest.fs
@@ -21,27 +21,27 @@ let prNl = printf "\n"
 let rule1 () = let x = newVar "x"
                Rule ((x, x), [])
 
-let rule2 () = let x, xx, y = newVar3 ("x", "xx", "y")
+let rule2 () = let x, xx, y = newVar3 ("2x", "2xx", "2y")
                Rule ((Mix(x,xx), y),  [(x,y)])
 
-let rule3 () = let x, xx, y = newVar3 ("x", "xx", "y")
+let rule3 () = let x, xx, y = newVar3 ("3x", "3xx", "3y")
                Rule ((Mix(x,xx), y),  [(xx,y)])
 
 let rule4 () = Rule ((Mix(A, B), Mix(B, A)), [])
 let rule5 () = Rule ((Mix(B, A), Mix(A, B)), [])
-let rule6 () = let x, xx, y, yy = newVar4 ("x", "xx", "y", "yy")
+let rule6 () = let x, xx, y, yy = newVar4 ("6x", "6xx", "6y", "6yy")
                Rule ((Mix(x,xx), Mix(y,yy)),  [(x,y); (xx,yy)])
 
-let rule7 () = let x, xx, y, yy = newVar4 ("x", "xx", "y", "yy")
+let rule7 () = let x, xx, y, yy = newVar4 ("7x", "7xx", "7y", "7yy")
                Rule ((Mix(x,xx), Mix(y,yy)),  [(x,yy); (xx,y)])
 
-let rule8 () = let x = newVar "x"
+let rule8 () = let x = newVar "8x"
                Rule ((x, Mix(x,x)),  [])
 
-let rule9 () = let x, xx, y, z = newVar4 ("x", "xx", "y", "z")
+let rule9 () = let x, xx, y, z = newVar4 ("9x", "9xx", "9y", "9z")
                Rule ( (Mix (x,xx),y),  [(x,z); (z,y)] )
 
-let rule10() = let x, xx, y, z = newVar4 ("x", "xx", "y", "z")
+let rule10() = let x, xx, y, z = newVar4 ("0x", "0xx", "0y", "0z")
                Rule ( (Mix (x,xx),y),  [(xx,z); (z,y)] )
 
 let rule11() = Rule ( (A,B),  [] )
@@ -54,7 +54,7 @@ let rulesC = [rule4; rule5; rule9; rule10; rule11; rule12]         // Rules 9,10
 
 
 let runTests () =
- 
+
     suffices [rule1] (A, A) |> prTest "suffices [rule1] (A, A)" true
     suffices [rule1] (A, B) |> prTest "suffices [rule1] (A, B)" false
     
@@ -75,7 +75,6 @@ let runTests () =
     suffices rulesB (Mix (Mix (B, B), B), Mix (B, B)) |> prTest "suffices rulesB (Mix (Mix (B, B), B), Mix (B, B))" true
     suffices rulesB (Mix (Mix (A, B), B), Mix (B, A)) |> prTest "suffices rulesB (Mix (Mix (A, B), B), Mix (B, A))" true
     prNl
-
 
     suffices rulesC (A, B) |> prTest "suffices rulesC (A, B)" true
     suffices rulesC (Mix (A, B), A) |> prTest "suffices rulesC (Mix (A, B),A)" true
